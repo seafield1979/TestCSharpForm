f5a005f678d3e040c88450a8a139f44827204a6a
seafield1979 <seafield1979@gmail.com>
2018-08-05 01:27:02 +0900

    FormScrollBar: LogView用のスクロール処理を実装

--- TestCSharpForm/FormScrollBar.cs
+++ TestCSharpForm/FormScrollBar.cs
@@ -19,7 +19,7 @@ namespace TestCSharpForm
 
         #region プロパティ
 
-        Document1 document1;
+        LVDocument1 document1;
         private bool isMouseDown;
         private Point mouseDownPos;
         private Point mouseOldPos;
@@ -30,34 +30,12 @@ namespace TestCSharpForm
 
         private void Initialize()
         {
-            document1 = new Document1(this.ClientSize.Width, this.ClientSize.Height, hScrollBar2, vScrollBar2);
+            document1 = new LVDocument1(this.ClientSize.Width, this.ClientSize.Height, hScrollBar2, vScrollBar2);
 
             // マウスホイールのイベント登録
             this.MouseWheel += new MouseEventHandler(this.MainForm_MouseWheel);
 
         }
-
-        public void UpdateScrollBarV(int value)
-        {
-            vScrollBar2.Value = value;
-        }
-
-        public void UpdateScrollBarH(int value)
-        {
-            hScrollBar2.Value = value;
-        }
-
-        private void ScrollX(int move)
-        {
-            
-        }
-
-        private void ScrollY(int move)
-        {
-            vScrollBar2.Value += move;
-            panel1.Invalidate();
-        }
-
         // zoom::
         private void ZoomUp()
         {
@@ -119,15 +97,11 @@ namespace TestCSharpForm
 
         private void vScrollBar2_Scroll(object sender, ScrollEventArgs e)
         {
-            ScrollBar sb = (ScrollBar)sender;
-            document1.UpdateSBV(sb.Value);
             panel1.Invalidate();
         }
 
         private void hScrollBar2_Scroll(object sender, ScrollEventArgs e)
         {
-            ScrollBar sb = (ScrollBar)sender;
-            document1.UpdateSBH(sb.Value);
             panel1.Invalidate();
         }
 
@@ -166,7 +140,6 @@ namespace TestCSharpForm
                 int moveY = e.Y - mouseOldPos.Y;
                 mouseOldPos.X = e.X;
                 mouseOldPos.Y = e.Y;
-                //Debug.WriteLine("mouse move {0} {1}", moveX, moveY); 
 
                 // ホイール量は e.Delta
                 if (document1.ScrollX(moveX) == true)
@@ -215,256 +188,6 @@ namespace TestCSharpForm
         #endregion イベント
 
     }
-
-    public struct SBInfo
-    {
-        public int max;
-        public int large;
-        public int value;
-
-        public void Init(int value, int max, int large)
-        {
-            this.value = value;
-            this.max = max;
-            this.large = large;
-        }
-    }
-
-    class Document1
-    {
-        //
-        // Consts
-        //
-        #region Consts
-
-        private int topMarginX = 100;
-        private int topMarginY = 100;
-        private int bottomMarginX = 100;
-        private int bottomMarginY = 100;
-
-        private int intervalX = 100;
-        private int intervalY = 100;
-
-        #endregion
-
-        //
-        // Properties
-        //
-        #region Properties
-        SBInfo sbV, sbH;
-
-        private Image image;                // LogView描画先のImage
-
-        HScrollBar scrollBarH;
-        VScrollBar scrollBarV;
-
-        float zoomRate = 1.0f;
-
-        #endregion
-
-        public Document1(int width, int height, HScrollBar scrollBarH, VScrollBar scrollBarV)
-        {
-
-            this.scrollBarH = scrollBarH;
-            this.scrollBarV = scrollBarV;
-            sbV = new SBInfo();
-            sbV.Init(0, 10000, height - (topMarginY + bottomMarginY));
-            sbH = new SBInfo();
-            sbH.Init(0, 10000, width - (topMarginX + bottomMarginX));
-
-            Resize(width, height);
-
-        }
-
-        // 
-        // Methods 
-        //
-        public void Resize(int width, int height)
-        {
-            if (image != null)
-            {
-                image.Dispose();
-            }
-
-            image = new Bitmap(width, height);
-
-            scrollBarH.LargeChange = sbH.large = width - (topMarginX + bottomMarginX);
-            scrollBarV.LargeChange = sbV.large = height - (topMarginY + bottomMarginY);
-
-            scrollBarH.LargeChange = sbH.large;
-            scrollBarH.Maximum = sbH.max;
-
-            scrollBarV.LargeChange = sbV.large;
-            scrollBarV.Maximum = sbV.max;
-
-        }
-
-        public void setZoomRate(float zoomRate)
-        {
-            this.zoomRate = zoomRate;
-        }
-
-        public bool ScrollX(int delta)
-        {
-            int oldValue = sbH.value;
-
-            sbH.value += delta;
-
-            if (sbH.value < 0)
-            {
-                sbH.value = 0;
-            }
-            if (sbH.value > sbH.max - sbH.large)
-            {
-                sbH.value = sbH.max - sbH.large;
-            }
-
-            if (oldValue != sbH.value)
-            {
-                scrollBarH.Value = sbH.value;
-                return true;
-            }
-            return false;
-        }
-
-        public bool ScrollY(int delta)
-        {
-            int oldValue = sbV.value;
-
-            sbV.value += delta;
-
-            if (sbV.value < 0)
-            {
-                sbV.value = 0;
-            }
-            if (sbV.value > sbV.max - sbV.large)
-            {
-                sbV.value = sbV.max - sbV.large;
-            }
-            if (oldValue != sbV.value)
-            {
-                scrollBarV.Value = sbV.value;
-                return true;
-            }
-            return false;
-        }
-
-        public bool ScrollDown()
-        {
-            return ScrollY(sbV.large);
-        }
-
-        public bool ScrollUp()
-        {
-            return ScrollY(-sbV.large);
-        }
-
-        public void Draw(Graphics g)
-        {
-            using (Graphics g2 = Graphics.FromImage(image))
-            {
-                // clear background
-                g2.FillRectangle(Brushes.Black, 0, 0, image.Width, image.Height);
-
-                var font1 = new Font("Arial", 12);
-
-                // テキスト表示
-                int x0 = 10;
-                int y0 = 10;
-
-                g2.DrawString(String.Format("value {0},{1} max {2},{3}", sbH.value, sbV.value, sbH.max, sbV.max), font1, Brushes.White, x0, y0);
-                y0 += 25;
-                g2.DrawString(String.Format("large {0}", sbH.large, sbV.large), font1, Brushes.White, x0, y0);
-                y0 += 25;
-                g2.DrawString(String.Format("zoomRate {0}", zoomRate), font1, Brushes.White, x0, y0);
-                y0 += 25;
-
-                // クリッピング設定
-                Rectangle rect1 = new Rectangle(topMarginX, topMarginY, sbH.large, sbV.large);
-                g2.SetClip(rect1);
-
-                g2.FillRectangle(Brushes.DarkRed, rect1);
-
-                int x = topMarginX + sbH.value - (sbH.value % intervalX);
-                int y = topMarginY + sbV.value - (sbV.value % intervalY);
-
-                // 横のライン
-                bool drawStr = true;
-                while ( y < sbV.value + sbV.large + intervalY)
-                {
-                    if (drawStr)
-                    {
-                        //drawStr = false;
-                        g2.DrawString(String.Format("{0}ms", (y - topMarginY) / zoomRate ), font1, Brushes.Yellow, topMarginX, y - sbV.value);
-                    }
-                    g2.DrawLine(Pens.White, topMarginX, y - sbV.value, image.Width - bottomMarginX, y - sbV.value);
-                    y += intervalY;
-                }
-                // 縦のライン
-                drawStr = true;
-                while ( x < sbH.value + sbH.large + intervalX)
-                {
-                    if (drawStr)
-                    {
-                        //drawStr = false;
-                        g2.DrawString(String.Format("{0}ms", (x - topMarginX) / zoomRate), font1, Brushes.Yellow, x - sbH.value, topMarginY);
-                    }
-                    g2.DrawLine(Pens.White, x - sbH.value, topMarginY, x - sbH.value, image.Height - bottomMarginY);
-                    x += intervalX;
-                }
-
-            }
-            g.DrawImage(image, 0, 0);
-        }
-
-        // Document側で表示情報を更新
-        public void UpdateSBV(int value)
-        {
-            // スクロールバーに反映
-            sbV.value = value;
-
-            scrollBarV.Value = value;
-            Debug.WriteLine(String.Format("sbV value:{0} max:{1} large:{2}", scrollBarV.Value, scrollBarV.Maximum, scrollBarV.LargeChange));
-        }
-
-        // Document側で表示情報を更新
-        public void UpdateSBH(int value)
-        {
-            // スクロールバーに反映
-            sbH.value = value;
-
-            scrollBarH.Value = value;
-            Debug.WriteLine(String.Format("sbH value:{0} max:{1} large:{2}", scrollBarH.Value, scrollBarH.Maximum, scrollBarH.LargeChange));
-        }
-
-
-        // zoom::
-        // 拡大
-        public bool ZoomUp()
-        {
-            zoomRate *= 1.2f;
-            ChangeZoomRate();
-            return true;
-        }
-
-        // 縮小
-        public bool ZoomDown()
-        {
-            zoomRate *= 0.8f;
-            ChangeZoomRate();
-            return true;
-        }
-
-        // 拡大率が変化したときの処理
-        private void ChangeZoomRate()
-        {
-            // 拡大したときの動作としてスクロールバーのmaxが変化するパターンと
-            // LargeChangeが変化するパターンがあるが、ここではmaxが変換するパターンを採用
-            scrollBarH.Maximum = sbV.max = (int)(10000.0f * zoomRate);
-        }
-    }
-
-
     public class DoubleBufferingPanel : System.Windows.Forms.Panel
     {
         public DoubleBufferingPanel()
---
+++ TestCSharpForm/LVDocument1.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TestCSharpForm
+{
+
+    //public struct SBInfo
+    //{
+    //    public int max;
+    //    public int large;
+    //    public int value;
+
+    //    public void Init(int value, int max, int large)
+    //    {
+    //        this.value = value;
+    //        this.max = max;
+    //        this.large = large;
+    //    }
+    //}
+
+
+    class LVDocument1
+    {
+        //
+        // Consts
+        //
+        #region Consts
+
+        private int topMarginX = 100;
+        private int topMarginY = 100;
+        private int bottomMarginX = 100;
+        private int bottomMarginY = 100;
+
+        private int intervalX = 100;
+        private int intervalY = 100;
+
+        #endregion
+
+        //
+        // Properties
+        //
+        #region Properties
+        
+        private double topTime;
+
+        public double TopTime
+        {
+            get { return topTime; }
+            set { topTime = value; }
+        }
+
+        private double endTime;
+
+        public double EndTime
+        {
+            get { return endTime; }
+            set { endTime = value; }
+        }
+
+        private double dispTopTime;
+
+        public double DispTopTime
+        {
+            get { return dispTopTime; }
+            set { dispTopTime = value; }
+        }
+
+        private double dispEndTime;
+
+        public double DispEndTime
+        {
+            get { return dispEndTime; }
+            set { dispEndTime = value; }
+        }
+
+
+        private float zoomRate = 1.0f;
+
+        public float ZoomRate
+        {
+            get { return zoomRate; }
+            set { zoomRate = value; }
+        }
+
+        private LogViewPixTime pixTime;
+
+
+        private Image image;                // LogView描画先のImage
+
+        HScrollBar scrollBarH;
+        VScrollBar scrollBarV;
+
+        #endregion
+
+        public LVDocument1(int width, int height, HScrollBar scrollBarH, VScrollBar scrollBarV)
+        {
+
+            this.scrollBarH = scrollBarH;
+            this.scrollBarV = scrollBarV;
+
+            pixTime = new LogViewPixTime();
+
+            topTime = 0.0;
+            endTime = 10.0;
+            dispTopTime = 0.0;
+            dispEndTime = GetDispEndTime();
+            
+            Resize(width, height);
+        }
+
+        // 
+        // Methods 
+        //
+        public void Resize(int width, int height)
+        {
+            if (image != null)
+            {
+                image.Dispose();
+            }
+
+            image = new Bitmap(width, height);
+
+            scrollBarH.LargeChange = width - (topMarginX + bottomMarginX);
+            scrollBarV.LargeChange = height - (topMarginY + bottomMarginY);
+
+            scrollBarH.Maximum = 1000;
+            scrollBarV.Maximum = (int)pixTime.timeToPix(endTime);
+        }
+
+        /**
+         * 表示領域の端の時間
+         * 画面に表示されるViewの一番端のピクセルの時間
+         */
+        private double GetDispEndTime()
+        {
+            return dispTopTime + pixTime.pixToTime(scrollBarV.LargeChange);
+        }
+
+        public void setZoomRate(float zoomRate)
+        {
+            this.zoomRate = zoomRate;
+        }
+
+        private void UpdateScrollY()
+        {
+            dispTopTime = topTime + pixTime.pixToTime(scrollBarV.Value);
+            dispEndTime = GetDispEndTime();
+            Debug.WriteLine("dispTopTime:{0} {1}", dispTopTime, scrollBarV.Value);
+        }
+
+        public bool ScrollX(int delta)
+        {
+            return ScrollXY(scrollBarH, delta);
+        }
+
+        public bool ScrollY(int delta)
+        {
+            bool ret = ScrollXY(scrollBarV, delta);
+            UpdateScrollY();
+            return ret;
+        }
+
+        public bool ScrollXY(ScrollBar sb, int delta)
+        {
+            int oldValue = sb.Value;
+
+            if (sb.Value + delta > sb.Maximum)
+            {
+                sb.Value = sb.Maximum;
+            }
+            else if (sb.Value + delta < 0)
+            {
+                sb.Value = 0;
+            }
+            else
+            {
+                sb.Value += delta;
+            }
+            Debug.WriteLine("scrollXY");
+
+            if (oldValue != sb.Value)
+            {   
+                return true;
+            }
+            return false;
+        }
+
+        public bool ScrollDown()
+        {
+            return ScrollY(scrollBarV.LargeChange);
+        }
+
+        public bool ScrollUp()
+        {
+            return ScrollY(-scrollBarV.LargeChange);
+        }
+
+        public void Draw(Graphics g)
+        {
+            using (Graphics g2 = Graphics.FromImage(image))
+            {
+                UpdateSBH(scrollBarH.Value);
+                UpdateSBV(scrollBarV.Value);
+
+                // clear background
+                g2.FillRectangle(Brushes.Black, 0, 0, image.Width, image.Height);
+
+                var font1 = new Font("Arial", 12);
+
+                // テキスト表示
+                int x0 = 10;
+                int y0 = 10;
+
+                g2.DrawString(String.Format("dispTopTime:{0} dispEndTime:{1}", dispTopTime, dispEndTime), font1, Brushes.White, x0, y0);
+                y0 += 20;
+                g2.DrawString(String.Format("[sbH] value:{0},large:{1} max:{2}", scrollBarH.Value, scrollBarV.LargeChange, scrollBarV.Maximum),
+                    font1, Brushes.White, x0, y0);
+                y0 += 20;
+                g2.DrawString(String.Format("[sbV] value:{0},large:{1} max:{2}", scrollBarV.Value, scrollBarV.LargeChange, scrollBarV.Maximum),
+                    font1, Brushes.White, x0, y0);
+                y0 += 20;
+                g2.DrawString(String.Format("zoomRate:{0} pixTime.zoom:{1}", zoomRate, pixTime.Val), font1, Brushes.White, x0, y0);
+                y0 += 20;
+
+                // クリッピング設定
+                Rectangle rect1 = new Rectangle(topMarginX, topMarginY, scrollBarH.LargeChange, scrollBarV.LargeChange);
+                g2.SetClip(rect1);
+
+                g2.FillRectangle(Brushes.DarkRed, rect1);
+
+                int x = -(scrollBarH.Value % intervalX);
+                int y = -(scrollBarV.Value % intervalY);
+
+                // 横のライン
+                while (y < scrollBarV.LargeChange)
+                {
+                    double time = dispTopTime + pixTime.pixToTime(y);
+                    string str = String.Format("{0}s", time);
+                    g2.DrawString(str,
+                        font1, Brushes.Yellow, topMarginX, topMarginY + y);
+
+                    if (str.Length > 4)
+                    {
+                        Debug.WriteLine("hoge");
+                    }
+                    
+                    g2.DrawLine(Pens.White, topMarginX, topMarginY + y,
+                        image.Width - bottomMarginX, topMarginY + y);
+                    y += intervalY;
+                }
+                // 縦のライン
+                while (x < scrollBarH.LargeChange)
+                {
+                    g2.DrawString(String.Format("{0}", topMarginX + x),
+                        font1, Brushes.Yellow, x - scrollBarH.Value, topMarginY);
+                    g2.DrawLine(Pens.White, topMarginX + x, topMarginY,
+                        topMarginX + x, image.Height - bottomMarginY);
+                    x += intervalX;
+                }
+
+            }
+            g.DrawImage(image, 0, 0);
+        }
+
+        // Document側で表示情報を更新
+        public void UpdateSBV(int value)
+        {
+            // スクロールバーに反映
+            scrollBarV.Value = value;
+            Debug.WriteLine("value:{0}", scrollBarV.Value);
+            UpdateScrollY();
+        }
+
+        // Document側で表示情報を更新
+        public void UpdateSBH(int value)
+        {
+            // スクロールバーに反映
+            scrollBarH.Value = value;
+            Debug.WriteLine(String.Format("sbH value:{0} max:{1} large:{2}", scrollBarH.Value, scrollBarH.Maximum, scrollBarH.LargeChange));
+        }
+
+        // zoom::
+        // 拡大
+        public bool ZoomUp()
+        {
+            zoomRate *= 1.2f;
+            ChangeZoomRate();
+            return true;
+        }
+
+        // 縮小
+        public bool ZoomDown()
+        {
+            zoomRate *= 0.8f;
+            ChangeZoomRate();
+            return true;
+        }
+
+        // 拡大率が変化したときの処理
+        private void ChangeZoomRate()
+        {
+            // 拡大したときの動作としてスクロールバーのmaxが変化するパターンと
+            // LargeChangeが変化するパターンがあるが、ここではmaxが変換するパターンを採用
+            scrollBarH.Maximum = (int)(10000.0f * zoomRate);
+        }
+    }
+
+    /**
+     * LogViewの1pixelあたりの表示時間
+     */
+    class LogViewPixTime
+    {
+        //
+        // Properties
+        //
+
+        // 1pixelあたりの時間
+        private double val;
+
+        public double Val
+        {
+            get { return val; }
+            set { val = value; }
+        }
+
+        //
+        // Constructor
+        //
+        public LogViewPixTime()
+        {
+            // 1000pix = 1sec -> 1pix = 0.001sec = 1ms
+            val = 0.001;
+        }
+
+        //
+        // Medhots
+        //
+        /**
+         * ズームイン
+         * 表示される領域が狭くなる(1pixあたりの時間が小さくなる)
+         * 1pix = 0.01秒 -> 1pix = 0.005秒
+         */
+        public void ZoomIn()
+        {
+            val *= 0.8f;
+        }
+
+        /**
+         * ズームアウト
+         * 表示される領域が広くなる(1pixあたりの時間が大きくなる)
+         * 1pix = 0.01秒 -> 1pix = 0.02秒
+         */
+        public void ZoomOut()
+        {
+            val *= 1.2f;
+        }
+
+        public override string ToString()
+        {
+            //return String.Format("1pix={0}sec", val);
+            return String.Format("1sec={0}pix", val);
+        }
+
+        /**
+         * 時間を現在のズーム率でpixelに変換する
+         * @input time : 変換元の時間(sec)
+         * @output 変換後のpixel数
+         */
+        public UInt64 timeToPix(double time)
+        {
+            // 1秒あたりのpixel数を取得してから時間(sec)を書ける
+            return (UInt64)((1.0 / val) * time);
+        }
+
+        /**
+         * pixelを現在のズーム率で時間に変換する
+         * @input pix : 変換元のpixel数
+         * @output 変換後の時間(sec)
+         */
+        public double pixToTime(int pix)
+        {
+            return pix * val;
+        }
+    }
+}
